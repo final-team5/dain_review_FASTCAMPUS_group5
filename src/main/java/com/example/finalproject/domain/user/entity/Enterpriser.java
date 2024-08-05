fd26910c4e6a11531f211b08e1dd6f84a2451c72
Reddyong <d0209hong@naver.com>
2024-08-05 17:22:28 +0900

    influencer 엔티티 추가

---
+++ src/main/java/com/example/finalproject/domain/user/entity/Enterpriser.java
@@ -0,0 +1,31 @@
+package com.example.finalproject.domain.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_enterpriser")
+@Entity
+public class Enterpriser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer seq;
+
+    @Column(length = 50)
+    private String company;
+
+    @Column(name = "business_number", length = 50)
+    private String businessNumber;
+
+    @Column(length = 50)
+    private String representative;
+
+    @Column(name = "attached_file", length = 50)
+    private String atta;
+}
--- src/main/java/com/example/finalproject/domain/user/entity/Influencer.java
+++ src/main/java/com/example/finalproject/domain/user/entity/Influencer.java
@@ -1,6 +1,7 @@
 package com.example.finalproject.domain.user.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,35 +19,52 @@ public class Influencer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
 
-
-    private String pw;
-    private String name;
-    private String phone;
-    private String signupSource;
-    private Integer postalCode;
-    private String address;
-    private String addressDetail;
-    private String profile;
-    private Integer point;
-    private Integer status;
-    private Integer cancel;
-    private Integer penalty;
-
+    @Column(length = 50)
     private String nickname;
+
     private Date birthdate;
+
     private Integer gender;
+
+    @Column(name = "blog_link", columnDefinition = "TEXT")
     private String blogLink;
+
+    @Column(name = "blog_rank", length = 50)
     private String blogRank;
+
+    @Column(name = "blog_visitors")
     private Integer blogVisitors;
+
+    @Column(name = "instagram_link", columnDefinition = "TEXT")
     private String instagramLink;
+
+    @Column(name = "instagram_rank", length = 50)
     private String instagramRank;
+
+    @Column(name = "instagram_follower")
     private Integer instagramFollower;
+
+    @Column(name = "youtube_link", columnDefinition = "TEXT")
     private String youtubeLink;
+
+    @Column(name = "youtube_rank", length = 50)
     private String youtubeRank;
+
+    @Column(name = "youtube_subscriber")
     private Integer youtubeSubscriber;
+
+    @Column(name = "tiktok_link", columnDefinition = "TEXT")
     private String tiktokLink;
+
+    @Column(name = "tiktok_rank", length = 50)
     private String tiktokRank;
+
+    @Column(name = "tiktok_follower")
     private Integer tiktokFollower;
+
+    @Column(name = "other_link", columnDefinition = "TEXT")
     private String otherLink;
+
+    @Column(name = "other_rank", length = 50)
     private String otherRank;
 }
