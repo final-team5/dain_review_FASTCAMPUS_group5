75a0c196b028b1d15c86c101297ee5ab3b7f3093
Reddyong <d0209hong@naver.com>
2024-08-05 17:35:07 +0900

    Agency 엔티티 추가 (광고 대행사 신청 리스트)

---
+++ src/main/java/com/example/finalproject/domain/user/entity/Agency.java
@@ -0,0 +1,28 @@
+package com.example.finalproject.domain.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_agency")
+@Entity
+public class Agency {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer seq;
+
+    @Column(columnDefinition = "TEXT")
+    private String reason;
+
+    private Integer status;
+
+    @Column(name = "create_date")
+    private Date createDate;
+}
