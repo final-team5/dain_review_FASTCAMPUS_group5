105669c0e60a41c926806b5fb5b20e68affed1a5
Reddyong <d0209hong@naver.com>
2024-08-05 18:21:34 +0900

    application.properties 수정

--- src/main/java/com/example/finalproject/domain/campaign/entity/Campaign.java
+++ src/main/java/com/example/finalproject/domain/campaign/entity/Campaign.java
@@ -23,4 +23,54 @@ public class Campaign {
     @Column(length = 50)
     private String category;
 
+    @Column(length = 50)
+    private String platform;
+
+    @Column(length = 50)
+    private String type;
+
+    @Column(length = 50)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String contents;
+
+    @Column(columnDefinition = "TEXT")
+    private String service;
+
+    @Column(name = "keyword_1", columnDefinition = "TEXT")
+    private String keyword1;
+
+    @Column(name = "keyword_2", columnDefinition = "TEXT")
+    private String keyword2;
+
+    @Column(name = "keyword_3", columnDefinition = "TEXT")
+    private String keyword3;
+
+    @Column(name = "campaign_link", columnDefinition = "TEXT")
+    private String campaignLink;
+
+    @Column(columnDefinition = "mission")
+    private String mission;
+
+    private Integer recruiter;
+
+    @Column(length = 50)
+    private String image;
+
+    @Column(length = 50)
+    private String city;
+
+    @Column(length = 50)
+    private String district;
+
+    private Integer point;
+
+    @Column(length = 50)
+    private String tag;
+
+    private Integer status;
+
+
+
 }
