0b1fbdb6bef1ed5696d4a99b65d1041ab4ed50a8
Reddyong <d0209hong@naver.com>
2024-08-05 18:03:59 +0900

    Alarm 엔티티 추가

---
+++ src/main/java/com/example/finalproject/domain/alarm/entity/Alarm.java
@@ -0,0 +1,34 @@
+package com.example.finalproject.domain.alarm.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "alarm")
+@Entity
+public class Alarm {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer seq;
+
+    @Column(name = "target_seq")
+    private Integer targetSeq;
+
+    @Column(name = "target_type")
+    private Integer targetType;
+
+    @Column(columnDefinition = "TEXT")
+    private String message;
+
+    @Column(name = "create_date")
+    private Date createDate;
+
+    private Integer check;
+}
