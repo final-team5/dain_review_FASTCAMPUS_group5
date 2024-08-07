4942b139709ec4028dccc19a5f201045040b7a30
Reddyong <d0209hong@naver.com>
2024-08-07 16:05:46 +0900

    feat : UserController
    * 게시판 댓글 수정 컨트롤러 기본 틀 작성.

--- src/main/java/com/example/finalproject/controller/UserController.java
+++ src/main/java/com/example/finalproject/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.finalproject.controller;
 
 import com.example.finalproject.domain.post.dto.PostCommentDto;
 import com.example.finalproject.domain.post.dto.request.PostCommentSaveRequest;
+import com.example.finalproject.domain.post.dto.request.PostCommentUpdateRequest;
 import com.example.finalproject.domain.post.dto.response.PostCommentResponse;
 import com.example.finalproject.domain.post.service.PostCommentService;
 import com.example.finalproject.global.util.ResponseApi;
@@ -9,10 +10,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Api(tags = "회원")
 @RequestMapping(path = "/user")
@@ -30,6 +28,19 @@ public class UserController {
             Integer userSeq
     ) {
         PostCommentDto postCommentDto = postCommentService.save(userSeq, postCommentSaveRequest.getPostSeq(), postCommentSaveRequest.getComment());
+        PostCommentResponse postCommentResponse = PostCommentResponse.from(postCommentDto);
+
+        return ResponseApi.success(HttpStatus.OK, postCommentResponse);
+    }
+
+    @ApiOperation(value = "커뮤니티 댓글 수정", tags = "사용자 - 커뮤니티")
+    @PutMapping(path = "/community/comments")
+    public ResponseApi<PostCommentResponse> updatePostComment(
+            @RequestBody PostCommentUpdateRequest postCommentUpdateRequest,
+            // TODO : security 도입 후 user 인자로 변경 예정
+            Integer userSeq
+    ) {
+        PostCommentDto postCommentDto = postCommentService.update(postCommentUpdateRequest.getPostSeq(), postCommentUpdateRequest.getPostCommentSeq(), postCommentUpdateRequest.getComment());
         PostCommentResponse postCommentResponse = PostCommentResponse.from(postCommentDto);
 
         return ResponseApi.success(HttpStatus.OK, postCommentResponse);
