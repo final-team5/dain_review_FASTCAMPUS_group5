47b819dfdf132f12ea1c59753e00bccde5eac1b7
Reddyong <d0209hong@naver.com>
2024-08-07 11:10:44 +0900

    feat : 게시판에 댓글 저장 기능 서비스 로직 구현.
    * 게시판 존재여부 확인.
    * 회원 ID 로 회원 조회.
    * 댓글 저장 후 PostCommentDto 반환.

--- src/main/java/com/example/finalproject/domain/post/service/PostCommentService.java
+++ src/main/java/com/example/finalproject/domain/post/service/PostCommentService.java
@@ -1,6 +1,10 @@
 package com.example.finalproject.domain.post.service;
 
+import com.example.finalproject.domain.post.dto.PostCommentDto;
+import com.example.finalproject.domain.post.entity.Post;
+import com.example.finalproject.domain.post.entity.PostComment;
 import com.example.finalproject.domain.post.repository.PostCommentRepository;
+import com.example.finalproject.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +14,23 @@ public class PostCommentService {
 
     private final PostCommentRepository postCommentRepository;
 
+    /**
+     * 게시글에 댓글 저장.
+     *
+     * @param userSeq : 회원 ID
+     * @param postSeq : 게시글 ID
+     * @param comment : 게시글 댓글
+     * @return PostCommentDto
+     */
+    public PostCommentDto save(Integer userSeq, Integer postSeq, String comment) {
+        // TODO : Post 존재 여부 체크
 
+        // TODO : userSeq 로 User 가져오기
+
+        // post comment save
+        PostComment postComment = PostComment.of(new User(), new Post(), comment);      // TODO : user, post 부분 채워넣기
+        PostComment savedPostComment = postCommentRepository.save(postComment);
+
+        return PostCommentDto.from(savedPostComment);
+    }
 }
