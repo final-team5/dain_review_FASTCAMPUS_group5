7eab5abef1ae7df3d269ac08dcecc3064cbafba8
Reddyong <d0209hong@naver.com>
2024-08-08 17:24:42 +0900

    feat : 게시판의 타입과 카테고리 관련 속성 enum 타입으로 수정.

--- src/main/java/com/example/finalproject/domain/post/entity/PostTypes.java
+++ src/main/java/com/example/finalproject/domain/post/entity/PostTypes.java
@@ -1,5 +1,6 @@
 package com.example.finalproject.domain.post.entity;
 
+import com.example.finalproject.domain.post.entity.enums.PostType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ public class PostTypes {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer seq;
 
+    @Enumerated(value = EnumType.STRING)
     @Column(length = 50)
-    private String type;
+    private PostType type;
 }
