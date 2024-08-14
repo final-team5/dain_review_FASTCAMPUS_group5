3c12315c6e648bfed4f9d6f0e6bc2912b53b6ab6
Reddyong <d0209hong@naver.com>
2024-08-14 17:00:04 +0900

    feat : 댓글 서비스 로직에 필요한 예외처리 에러 코드 추가.

--- src/main/java/com/example/finalproject/global/exception/error/ValidErrorCode.java
+++ src/main/java/com/example/finalproject/global/exception/error/ValidErrorCode.java
@@ -8,7 +8,16 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ValidErrorCode implements ErrorCode {
 
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다.")
+    // Post
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다."),
+
+    // User
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원 정보를 찾을 수 없습니다."),
+
+    // PostComment
+    POST_COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "작성된 댓글 정보가 존재하지 않습니다."),
+    POST_COMMENT_USER_MISMATCH(HttpStatus.BAD_REQUEST, "댓글 작성자와 일치하지 않습니다."),
+    POST_COMMENT_POST_MISMATCH(HttpStatus.BAD_REQUEST, "해당 게시글에 존재하지 않는 댓글입니다.")
     ;
 
     private final HttpStatus code;
