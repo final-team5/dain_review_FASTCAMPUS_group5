69e4eac57ee365c962e4addb420d2aea5bcf96c6
Reddyong <d0209hong@naver.com>
2024-08-19 17:52:47 +0900

    feat : 답글 기능 전체 조회 시 부모 댓글들만 조회하여 답글리스트로 조회하기 위해 쿼리 추가.

--- src/main/java/com/example/finalproject/domain/post/repository/PostCommentRepository.java
+++ src/main/java/com/example/finalproject/domain/post/repository/PostCommentRepository.java
@@ -5,8 +5,10 @@ import com.example.finalproject.domain.post.entity.PostComment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostCommentRepository extends JpaRepository<PostComment, Integer> {
 
+    @Query(value = "SELECT pc FROM PostComment pc WHERE pc.commentSeq is NULL")
     Page<PostComment> findAllByPost(Post post, Pageable pageable);
 }
