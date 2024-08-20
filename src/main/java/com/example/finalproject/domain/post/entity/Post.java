e24f2bdcba11692217f907b7508df88ebd888221
Reddyong <d0209hong@naver.com>
2024-08-20 12:04:10 +0900

    feat : User, PostCategories, PostTypes 와의 연관관계 매핑 및 soft delete 를 위한 설정 및 속성 추가.

--- src/main/java/com/example/finalproject/domain/post/entity/Post.java
+++ src/main/java/com/example/finalproject/domain/post/entity/Post.java
@@ -1,15 +1,21 @@
 package com.example.finalproject.domain.post.entity;
 
+import com.example.finalproject.domain.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.sql.Timestamp;
+import java.time.Instant;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@SQLDelete(sql = "UPDATE final.posts SET deleted_at = NOW() WHERE seq=?")
+@Where(clause = "deleted_at is NULL")
 @Table(name = "posts")
 @Entity
 public class Post {
@@ -17,6 +23,18 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer seq;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_seq")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_seq")
+    private PostCategories postCategories;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "type_seq")
+    private PostTypes postTypes;
 
     @Column(length = 50)
     private String title;
@@ -29,6 +47,22 @@ public class Post {
 
     private Integer status;
 
-    @Column(name = "create_date")
-    private Date createDate;
+    @Column(name = "registered_at")
+    private Timestamp registeredAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+
+    @PrePersist
+    void registeredAt() {
+        this.registeredAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
 }
