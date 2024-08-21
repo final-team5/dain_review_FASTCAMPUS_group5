891098422e4451488fd5f794992cae4bb362efea
Reddyong <d0209hong@naver.com>
2024-08-21 10:51:58 +0900

    feat : 서이추/맞팔 게시글 등록 시 서비스 로직 구현.
    * userSeq 를 통해 user 가져온다.
    * request 정보로 받은 sns 종류를 통해 PostTypes 객체 불러온다.
    * 서이추/맞팔 게시판이기 때문에, 맞는 seq 와 enum 값을 통해 PostCategories 객체 생성한다.
    * Post 객체 생성하여 저장한다.

--- src/main/java/com/example/finalproject/domain/post/service/PostService.java
+++ src/main/java/com/example/finalproject/domain/post/service/PostService.java
@@ -1,14 +1,65 @@
 package com.example.finalproject.domain.post.service;
 
+import com.example.finalproject.domain.post.dto.PostDto;
+import com.example.finalproject.domain.post.entity.Post;
+import com.example.finalproject.domain.post.entity.PostCategories;
+import com.example.finalproject.domain.post.entity.PostTypes;
+import com.example.finalproject.domain.post.entity.enums.PostCategory;
+import com.example.finalproject.domain.post.entity.enums.PostType;
 import com.example.finalproject.domain.post.repository.PostRepository;
+import com.example.finalproject.domain.post.repository.PostTypesRepository;
+import com.example.finalproject.domain.user.entity.User;
+import com.example.finalproject.domain.user.repository.UserRepository;
+import com.example.finalproject.global.exception.error.ValidErrorCode;
+import com.example.finalproject.global.exception.type.ValidException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class PostService {
 
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final PostTypesRepository postTypesRepository;
 
+    @Transactional
+    public PostDto saveFollowPost(PostType category, String contents, String title, Integer userSeq) {
+        User user = getUserOrException(userSeq);
 
+        PostTypes postTypes = getPostTypesOrException(category);
+
+        PostCategories postCategories = PostCategories.of(4, PostCategory.EACH_OTHER_NEIGHBOR_FOLLOW);
+
+        Post post = Post.of(user, postCategories, postTypes, title, contents, 0);
+
+        Post savedPost = postRepository.save(post);
+
+        return PostDto.from(savedPost);
+    }
+
+    /**
+     * 회원 정보 존재 여부 체크
+     *
+     * @param userSeq : 회원 ID
+     * @return User
+     */
+    private User getUserOrException(Integer userSeq) {
+        return userRepository.findById(userSeq).orElseThrow(
+                () -> new ValidException(ValidErrorCode.USER_NOT_FOUND)
+        );
+    }
+
+    /**
+     * 회원 정보 존재 여부 체크
+     *
+     * @param category : sns 카테고리
+     * @return PostTypes
+     */
+    private PostTypes getPostTypesOrException(PostType category) {
+        return postTypesRepository.findByType(category).orElseThrow(
+                () -> new ValidException(ValidErrorCode.POST_TYPE_NOT_FOUND)
+        );
+    }
 }
