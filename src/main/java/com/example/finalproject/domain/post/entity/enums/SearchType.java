8d7b6d0ce8e6d32a1a963f30bee9105c5665887b
Reddyong <d0209hong@naver.com>
2024-08-25 13:07:01 +0900

    feat : 서이추/맞팔 게시판 전체 조회 기능에 필요한 검색 enum 타입 클래스 생성.

---
+++ src/main/java/com/example/finalproject/domain/post/entity/enums/SearchType.java
@@ -0,0 +1,15 @@
+package com.example.finalproject.domain.post.entity.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SearchType {
+    TITLE("제목"),
+    CONTENTS("내용"),
+    USER("작성자")
+    ;
+
+    private final String info;
+}
