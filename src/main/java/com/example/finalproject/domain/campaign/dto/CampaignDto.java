e2664911c1d3a574bc52e6141146c63b2c98f768
Reddyong <d0209hong@naver.com>
2024-08-26 22:00:09 +0900

    feat : 체험단 찜 기능 관련 dto 기본 틀 작성.

--- src/main/java/com/example/finalproject/domain/campaign/dto/CampaignDto.java
+++ src/main/java/com/example/finalproject/domain/campaign/dto/CampaignDto.java
@@ -14,5 +14,5 @@ import lombok.NoArgsConstructor;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CampaignDto {
 
-
+    private Integer seq;
 }
--- src/main/java/com/example/finalproject/domain/campaign/dto/response/CampaignPreferenceSaveResponse.java
+++ src/main/java/com/example/finalproject/domain/campaign/dto/response/CampaignPreferenceSaveResponse.java
@@ -14,5 +14,5 @@ import lombok.NoArgsConstructor;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CampaignPreferenceSaveResponse {
 
-
+    private Integer seq;
 }
