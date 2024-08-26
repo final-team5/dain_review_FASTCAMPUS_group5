52ff0958c3c5000c12f7f5e6914e5f6601cf499c
Reddyong <d0209hong@naver.com>
2024-08-26 22:01:14 +0900

    refactor : UserRepository 의 findByUsername 을 findByName 으로 수정.
    * 회원 테이블의 이름속성은 name.

--- src/main/java/com/example/finalproject/domain/user/service/UserService.java
+++ src/main/java/com/example/finalproject/domain/user/service/UserService.java
@@ -42,7 +42,7 @@ public class UserService {
 	}
 
 	public UserInfo findByUsername(String username) {
-		User user = userRepository.findByUsername(username)
+		User user = userRepository.findByName(username)
 				.orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
 
 		return UserInfo.builder()
