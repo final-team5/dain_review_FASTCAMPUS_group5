318b7e86149ce0c771c116f7c41f43f091eccb0d
Reddyong <d0209hong@naver.com>
2024-08-26 22:43:33 +0900

    feat : 체험단 찜 하기 기능에 대한 컨트롤러 코드 구현.

--- src/main/java/com/example/finalproject/controller/UserController.java
+++ src/main/java/com/example/finalproject/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.finalproject.controller;
 
+import com.example.finalproject.domain.campaign.dto.CampaignPreferenceDto;
 import com.example.finalproject.domain.campaign.dto.request.CampaignPreferenceSaveRequest;
 import com.example.finalproject.domain.campaign.dto.response.CampaignPreferenceSaveResponse;
+import com.example.finalproject.domain.campaign.service.CampaignService;
 import com.example.finalproject.domain.post.dto.PostCommentDto;
 import com.example.finalproject.domain.post.dto.PostDto;
 import com.example.finalproject.domain.post.dto.request.*;
@@ -31,6 +33,7 @@ public class UserController {
 
     private final PostCommentService postCommentService;
     private final PostService postService;
+    private final CampaignService campaignService;
 
     @ApiOperation(value = "커뮤니티 댓글 추가", tags = "사용자 - 커뮤니티")
     @PostMapping(path = "/community/comments")
@@ -159,7 +162,9 @@ public class UserController {
             // TODO : security 도입 후 user 인자로 변경 예정
             Integer userSeq
     ) {
+        CampaignPreferenceDto campaignPreferenceDto = campaignService.saveCampaignPreference(campaignPreferenceSaveRequest.getId(), userSeq);
+        CampaignPreferenceSaveResponse campaignPreferenceSaveResponse = CampaignPreferenceSaveResponse.from(campaignPreferenceDto);
 
-        return ResponseApi.success(HttpStatus.OK, null);
+        return ResponseApi.success(HttpStatus.OK, campaignPreferenceSaveResponse);
     }
 }
