3acaf8e45cfba72b85ad6405ad7c1b83372f8417
Reddyong <d0209hong@naver.com>
2024-08-26 22:41:03 +0900

    feat : 체험단 dto 작성.
    * 엔티티에서 응답으로 변환을 위한 모든 속성을 가진 dto 클래스.

--- src/main/java/com/example/finalproject/domain/campaign/dto/CampaignDto.java
+++ src/main/java/com/example/finalproject/domain/campaign/dto/CampaignDto.java
@@ -1,11 +1,17 @@
 package com.example.finalproject.domain.campaign.dto;
 
+import com.example.finalproject.domain.campaign.entity.Campaign;
+import com.example.finalproject.domain.user.dto.UserDto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.google.api.client.util.DateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -15,4 +21,148 @@ import lombok.NoArgsConstructor;
 public class CampaignDto {
 
     private Integer seq;
+
+    private UserDto userDto;
+
+    private String id;
+
+    private String category;
+
+    private String platform;
+
+    private String type;
+
+    private String title;
+
+    private String contents;
+
+    private String service;
+
+    private String keyword1;
+
+    private String keyword2;
+
+    private String keyword3;
+
+    private String campaignLink;
+
+    private String mission;
+
+    private Integer recruiter;
+
+    private String image;
+
+    private String city;
+
+    private String district;
+
+    private Integer point;
+
+    private String tag;
+
+    private Integer status;
+
+    private Date applicationStartDate;
+
+    private Date applicationEndDate;
+
+    private Date experienceStartDate;
+
+    private Date experienceEndDate;
+
+    private DateTime experienceStartTime;
+
+    private DateTime experienceEndTime;
+
+    private Integer monday;
+    private Integer tuesday;
+    private Integer wednesday;
+    private Integer thursday;
+    private Integer friday;
+    private Integer saturday;
+    private Integer sunday;
+
+    private Integer hashtag;
+    private Integer map;
+    private Integer link;
+
+    private Integer characters;
+
+    private Integer videoLength;
+
+    private Integer pictureCount;
+
+    private Integer accountTag;
+
+    private Integer sound;
+
+    private Integer advertisingDisplay;
+
+    private Timestamp registeredAt;
+
+    private Timestamp updatedAt;
+
+    public static CampaignDto of(Integer seq, UserDto userDto, String id, String category, String platform, String type, String title, String contents, String service,
+                                 String keyword1, String keyword2, String keyword3, String campaignLink, String mission, Integer recruiter, String image, String city, String district,
+                                 Integer point, String tag, Integer status, Date applicationStartDate, Date applicationEndDate, Date experienceStartDate, Date experienceEndDate,
+                                 DateTime experienceStartTime, DateTime experienceEndTime, Integer monday, Integer tuesday, Integer wednesday, Integer thursday, Integer friday, Integer saturday, Integer sunday,
+                                 Integer hashtag, Integer map, Integer link, Integer characters, Integer videoLength, Integer pictureCount, Integer accountTag, Integer sound,
+                                 Integer advertisingDisplay, Timestamp registeredAt, Timestamp updatedAt) {
+        return new CampaignDto(seq, userDto, id, category, platform, type, title, contents, service,
+                keyword1, keyword2, keyword3, campaignLink, mission, recruiter, image, city, district,
+                point, tag, status, applicationStartDate, applicationEndDate, experienceStartDate, experienceEndDate,
+                experienceStartTime, experienceEndTime, monday, tuesday, wednesday, thursday, friday, saturday, sunday,
+                hashtag, map, link, characters, videoLength, pictureCount, accountTag, sound,
+                advertisingDisplay, registeredAt, updatedAt);
+    }
+
+    public static CampaignDto from(Campaign campaign) {
+        return CampaignDto.of(
+                campaign.getSeq(),
+                UserDto.from(campaign.getUser()),
+                campaign.getId(),
+                campaign.getCategory(),
+                campaign.getPlatform(),
+                campaign.getType(),
+                campaign.getTitle(),
+                campaign.getContents(),
+                campaign.getService(),
+                campaign.getKeyword1(),
+                campaign.getKeyword2(),
+                campaign.getKeyword3(),
+                campaign.getCampaignLink(),
+                campaign.getMission(),
+                campaign.getRecruiter(),
+                campaign.getImage(),
+                campaign.getCity(),
+                campaign.getDistrict(),
+                campaign.getPoint(),
+                campaign.getTag(),
+                campaign.getStatus(),
+                campaign.getApplicationStartDate(),
+                campaign.getApplicationEndDate(),
+                campaign.getExperienceStartDate(),
+                campaign.getExperienceEndDate(),
+                campaign.getExperienceStartTime(),
+                campaign.getExperienceEndTime(),
+                campaign.getMonday(),
+                campaign.getTuesday(),
+                campaign.getWednesday(),
+                campaign.getThursday(),
+                campaign.getFriday(),
+                campaign.getSaturday(),
+                campaign.getSunday(),
+                campaign.getHashtag(),
+                campaign.getMap(),
+                campaign.getLink(),
+                campaign.getCharacters(),
+                campaign.getVideoLength(),
+                campaign.getPictureCount(),
+                campaign.getAccountTag(),
+                campaign.getSound(),
+                campaign.getAdvertisingDisplay(),
+                campaign.getRegisteredAt(),
+                campaign.getUpdatedAt()
+        );
+    }
 }
