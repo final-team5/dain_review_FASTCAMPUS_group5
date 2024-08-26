6973431ddcc0ef6634f81aceae1d320ea170499a
Reddyong <d0209hong@naver.com>
2024-08-26 16:02:46 +0900

    feat : 체험단 찜 기능을 구현하기 위한 엔티티 속성 추가.
    * user, campaign 엔티티와의 연관관계 일대다 매핑 완료.

--- src/main/java/com/example/finalproject/domain/campaign/entity/CampaignPreference.java
+++ src/main/java/com/example/finalproject/domain/campaign/entity/CampaignPreference.java
@@ -1,5 +1,6 @@
 package com.example.finalproject.domain.campaign.entity;
 
+import com.example.finalproject.domain.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,4 +17,12 @@ public class CampaignPreference {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer seq;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_seq")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "campaign_seq")
+    private Campaign campaign;
 }
