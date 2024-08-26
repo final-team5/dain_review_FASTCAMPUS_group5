209f3961840708c80ad27d66f6f34e9a6f400bac
Reddyong <d0209hong@naver.com>
2024-08-26 22:43:20 +0900

    feat : 체험단 찜 하기 기능에 대한 서비스 로직 구현.
    * 사용자 존재여부 검증.
    * 체험단 존재 여부 검증.
    * 찜 기능 수행.

--- src/main/java/com/example/finalproject/domain/campaign/service/CampaignService.java
+++ src/main/java/com/example/finalproject/domain/campaign/service/CampaignService.java
@@ -1,7 +1,13 @@
 package com.example.finalproject.domain.campaign.service;
 
 import com.example.finalproject.domain.campaign.dto.CampaignDto;
+import com.example.finalproject.domain.campaign.dto.CampaignPreferenceDto;
+import com.example.finalproject.domain.campaign.entity.Campaign;
+import com.example.finalproject.domain.campaign.entity.CampaignPreference;
 import com.example.finalproject.domain.campaign.repository.CampaignPreferenceRepository;
+import com.example.finalproject.domain.campaign.repository.CampaignRepository;
+import com.example.finalproject.domain.user.entity.User;
+import com.example.finalproject.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,15 +17,25 @@ import org.springframework.transaction.annotation.Transactional;
 public class CampaignService {
 
     private final CampaignPreferenceRepository campaignPreferenceRepository;
+    private final UserRepository userRepository;
+    private final CampaignRepository campaignRepository;
 
+    /**
+     * 체험단 찜 하기 기능.
+     *
+     * @param campaignSeq : 찜 할 체험단 ID
+     * @param userSeq : 로그인한 사용자 ID
+     * @return : CampaignPreferenceDto
+     */
     @Transactional
-    public CampaignDto saveCampaignPreference(Integer campaignSeq, Integer userSeq) {
-        // TODO : 사용자 존재여부 검증
+    public CampaignPreferenceDto saveCampaignPreference(Integer campaignSeq, Integer userSeq) {
+        User user = userRepository.getUserBySeqOrException(userSeq);
 
-        // TODO : 체험단 존재여부 검증
+        Campaign campaign = campaignRepository.getCampaignBySeqOrException(campaignSeq);
 
-        // TODO : 찜 저장
+        CampaignPreference campaignPreference = CampaignPreference.of(user, campaign);
+        CampaignPreference savedCampaignPreference = campaignPreferenceRepository.save(campaignPreference);
 
-        return null;
+        return CampaignPreferenceDto.from(savedCampaignPreference);
     }
 }
