a9945b3e403dabfcff65595a8d598a41a25e414a
Reddyong <d0209hong@naver.com>
2024-08-27 21:34:41 +0900

    feat : 찜 전체 목록 리스트 조회 기능에 대한 서비스 로직 작성.
    * 페이징 적용하여 반환시켜줌.

--- src/main/java/com/example/finalproject/domain/campaign/service/CampaignService.java
+++ src/main/java/com/example/finalproject/domain/campaign/service/CampaignService.java
@@ -1,7 +1,8 @@
 package com.example.finalproject.domain.campaign.service;
 
-import com.example.finalproject.domain.campaign.dto.CampaignDto;
 import com.example.finalproject.domain.campaign.dto.CampaignPreferenceDto;
+import com.example.finalproject.domain.campaign.dto.CampaignWithApplicantCountDto;
+import com.example.finalproject.domain.campaign.entity.CampaignWithApplicantCount;
 import com.example.finalproject.domain.campaign.entity.Campaign;
 import com.example.finalproject.domain.campaign.entity.CampaignPreference;
 import com.example.finalproject.domain.campaign.repository.CampaignPreferenceRepository;
@@ -9,6 +10,8 @@ import com.example.finalproject.domain.campaign.repository.CampaignRepository;
 import com.example.finalproject.domain.user.entity.User;
 import com.example.finalproject.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,5 +40,13 @@ public class CampaignService {
         CampaignPreference savedCampaignPreference = campaignPreferenceRepository.save(campaignPreference);
 
         return CampaignPreferenceDto.from(savedCampaignPreference);
+    }
+
+    public Page<CampaignWithApplicantCountDto> getCampaignPreferenceList(Integer userSeq, Pageable pageable) {
+        User user = userRepository.getUserBySeqOrException(userSeq);
+
+        Page<CampaignWithApplicantCount> campaignWithApplicantCounts = campaignRepository.findAllByUserAndStatus(user, 3, pageable);
+
+        return campaignWithApplicantCounts.map(CampaignWithApplicantCountDto::from);
     }
 }
