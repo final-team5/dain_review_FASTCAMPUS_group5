b07009b53ba42e57f5d8865a7649fd540d45433e
Reddyong <d0209hong@naver.com>
2024-08-27 11:42:24 +0900

    feat : 체험단 찜 제거 기능을 위해 등록, 수정, 삭제 시간 속성 추가 및 soft delete 기능 추가.

--- src/main/java/com/example/finalproject/domain/campaign/entity/CampaignPreference.java
+++ src/main/java/com/example/finalproject/domain/campaign/entity/CampaignPreference.java
@@ -4,12 +4,18 @@ import com.example.finalproject.domain.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@SQLDelete(sql = "UPDATE campaign_preference SET deleted_at = NOW() WHERE seq=?")
+@Where(clause = "deleted_at is NULL")
 @Table(name = "campaign_preference")
 @Entity
 public class CampaignPreference {
@@ -26,6 +32,15 @@ public class CampaignPreference {
     @JoinColumn(name = "campaign_seq")
     private Campaign campaign;
 
+    @Column(name = "registered_at")
+    private Timestamp registeredAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+
     public CampaignPreference(User user, Campaign campaign) {
         this.user = user;
         this.campaign = campaign;
@@ -33,5 +48,15 @@ public class CampaignPreference {
 
     public static CampaignPreference of(User user, Campaign campaign) {
         return new CampaignPreference(user, campaign);
+    }
+
+    @PrePersist
+    void registeredAt() {
+        this.registeredAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
     }
 }
