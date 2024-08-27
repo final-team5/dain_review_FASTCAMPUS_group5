2a4ed432661ade3e77dfa26fcf63bc50824fa8b2
Reddyong <d0209hong@naver.com>
2024-08-27 11:43:14 +0900

    feat : 체험단 정보를 통해 찜 했던 내역 조회 기능 메서드 추가.

--- src/main/java/com/example/finalproject/domain/campaign/repository/CampaignPreferenceRepository.java
+++ src/main/java/com/example/finalproject/domain/campaign/repository/CampaignPreferenceRepository.java
@@ -1,7 +1,20 @@
 package com.example.finalproject.domain.campaign.repository;
 
+import com.example.finalproject.domain.campaign.entity.Campaign;
 import com.example.finalproject.domain.campaign.entity.CampaignPreference;
+import com.example.finalproject.global.exception.error.ValidErrorCode;
+import com.example.finalproject.global.exception.type.ValidException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CampaignPreferenceRepository extends JpaRepository<CampaignPreference, Integer> {
+
+    Optional<CampaignPreference> findByCampaign(Campaign campaign);
+
+    default CampaignPreference getCampaignPreferenceByCampaignOrException(Campaign campaign) {
+        return findByCampaign(campaign).orElseThrow(
+                () -> new ValidException(ValidErrorCode.CAMPAIGN_PREFERENCE_NOT_FOUND)
+        );
+    }
 }
