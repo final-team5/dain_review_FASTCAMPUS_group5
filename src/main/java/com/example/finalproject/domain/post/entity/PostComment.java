e24cc2610477627ae40bd4611e9b63975d663583
Reddyong <d0209hong@naver.com>
2024-08-28 18:53:15 +0900

    refactor : 배포환경과 로컬 환경 분리 테스트.

--- src/main/java/com/example/finalproject/domain/post/entity/PostComment.java
+++ src/main/java/com/example/finalproject/domain/post/entity/PostComment.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@SQLDelete(sql = "UPDATE post_comments SET deleted_at = NOW() where seq=?")
+@SQLDelete(sql = "UPDATE ${schema}.post_comments SET deleted_at = NOW() where seq=?")
 @Where(clause = "deleted_at is NULL")
 @Table(name = "post_comments")
 @Entity
