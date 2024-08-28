5c042d7b0b605208e234a926dc7e642db070dc24
Reddyong <d0209hong@naver.com>
2024-08-28 15:14:56 +0900

    refactor : PostWithCommentsDto 댓글리스트 속성 페이지 속성으로 변경.

--- src/main/java/com/example/finalproject/domain/post/dto/PostWithCommentsDto.java
+++ src/main/java/com/example/finalproject/domain/post/dto/PostWithCommentsDto.java
@@ -1,15 +1,14 @@
 package com.example.finalproject.domain.post.dto;
 
 import com.example.finalproject.domain.post.entity.Post;
+import com.example.finalproject.domain.post.entity.PostComment;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.data.domain.Page;
 
 @Data
 @NoArgsConstructor
@@ -20,16 +19,17 @@ public class PostWithCommentsDto {
 
     private PostDto postDto;
 
-    private List<PostCommentDto> commentDtoList;
+    private Page<PostCommentDto> commentDtoList;
 
-    public static PostWithCommentsDto of(PostDto postDto, List<PostCommentDto> commentDtoList) {
+    public static PostWithCommentsDto of(PostDto postDto, Page<PostCommentDto> commentDtoList) {
         return new PostWithCommentsDto(postDto, commentDtoList);
     }
 
-    public static PostWithCommentsDto from(Post post) {
+    public static PostWithCommentsDto from(Post post, Page<PostComment> postComments) {
         return PostWithCommentsDto.of(
                 PostDto.from(post),
-                post.getComments().stream().map(PostCommentDto::from).collect(Collectors.toList())
+                postComments.map(PostCommentDto::from)
         );
     }
+
 }
