e65c2cdd340ec282f45cde711f714fa9f3f57416
Reddyong <d0209hong@naver.com>
2024-09-03 17:31:33 +0900

    refactor : 회원가입 시 생성, 수정, 삭제 시간에 대한 속성 추가 및 soft delete 기능 구현.

--- src/main/java/com/example/finalproject/domain/user/entity/User.java
+++ src/main/java/com/example/finalproject/domain/user/entity/User.java
@@ -5,13 +5,19 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@SQLDelete(sql = "UPDATE users SET deleted_at = NOW() WHERE seq=?")
+@Where(clause = "deleted_at is NULL")
 @Table(name = "users")
 @Builder
 @Entity
@@ -69,4 +75,48 @@ public class User {
     private Integer penalty;
 
     private Integer type;
+
+    @Column(name = "registered_at")
+    private Timestamp registeredAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+
+    public User(String email, String id, String pw, String role, String name, String phone, String profile, Integer loginType, Date createDate, String signupSource, Integer postalCode, String address, String addressDetail, Integer point, Integer status, Integer cancel, Integer penalty, Integer type) {
+        this.email = email;
+        this.id = id;
+        this.pw = pw;
+        this.role = role;
+        this.name = name;
+        this.phone = phone;
+        this.profile = profile;
+        this.loginType = loginType;
+        this.createDate = createDate;
+        this.signupSource = signupSource;
+        this.postalCode = postalCode;
+        this.address = address;
+        this.addressDetail = addressDetail;
+        this.point = point;
+        this.status = status;
+        this.cancel = cancel;
+        this.penalty = penalty;
+        this.type = type;
+    }
+
+    public static User of(String email, String id, String pw, String role, String name, String phone, String profile, Integer loginType, Date createDate, String signupSource, Integer postalCode, String address, String addressDetail, Integer point, Integer status, Integer cancel, Integer penalty, Integer type) {
+        return new User(email, id, pw, role, name, phone, profile, loginType, createDate, signupSource, postalCode, address, addressDetail, point, status, cancel, penalty, type);
+    }
+
+    @PrePersist
+    void registeredAt() {
+        this.registeredAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
 }
