1607a0504a4f0ca5d157c861c768df9f071c6071
Reddyong <d0209hong@naver.com>
2024-09-03 17:34:34 +0900

    feat : 인플루언서 회원가입 기능 서비스 로직 구현.

--- src/main/java/com/example/finalproject/domain/user/service/UserService.java
+++ src/main/java/com/example/finalproject/domain/user/service/UserService.java
@@ -1,20 +1,34 @@
 package com.example.finalproject.domain.user.service;
 
 import com.example.finalproject.domain.user.dto.Register;
+import com.example.finalproject.domain.user.dto.UserDto;
+import com.example.finalproject.domain.user.dto.UserInfluencerDto;
+import com.example.finalproject.domain.user.dto.request.InfluencerSignup;
 import com.example.finalproject.domain.user.entity.Businesses;
 import com.example.finalproject.domain.user.entity.Influencer;
 import com.example.finalproject.domain.user.entity.User;
 import com.example.finalproject.domain.user.dto.UserInfo;
+import com.example.finalproject.domain.user.entity.enums.SignUpSourceType;
 import com.example.finalproject.domain.user.repository.BusinessesRepository;
 import com.example.finalproject.domain.user.repository.InfluencerRepository;
 import com.example.finalproject.domain.user.repository.UserRepository;
+import com.example.finalproject.global.exception.error.AuthErrorCode;
+import com.example.finalproject.global.exception.error.ValidErrorCode;
+import com.example.finalproject.global.exception.type.AuthException;
+import com.example.finalproject.global.exception.type.ValidException;
+import com.example.finalproject.global.jwt.TokenProvider;
 import com.example.finalproject.global.util.FileUtils;
+
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +37,8 @@ public class UserService {
 	private final UserRepository userRepository;
 	private final InfluencerRepository influencerRepository;
 	private final BusinessesRepository businessesRepository;
+	private final TokenProvider tokenProvider;
+	private final PasswordEncoder passwordEncoder;
 
 	public UserInfo getUser(String email, Integer loginType) {
 		User user = userRepository.getByEmailAndType(email, loginType);
@@ -120,7 +136,7 @@ public class UserService {
 			influencerRepository.save(Influencer.builder()
 				.user(user)
 				.nickname(register.getNickname())
-				.gender(String.valueOf(register.getGender()))
+				.gender(register.getGender())
 				.birthdate(LocalDate.parse(register.getBirthdate(), DateTimeFormatter.ofPattern("yyyy-MM-dd")))
 				.blogLink(register.getBlog())
 				.instagramLink(register.getInstagram())
@@ -137,5 +153,73 @@ public class UserService {
 		); }
 	}
 
+
+	@Transactional
+	public UserInfluencerDto registerInfluencer(InfluencerSignup influencerSignup, MultipartFile profile) {
+		if (checkEmail(influencerSignup.getEmail())) {
+			throw new AuthException(AuthErrorCode.EMAIL_ALREADY_IN_USE);
+		}
+
+		if (checkNickname(influencerSignup.getNickname())) {
+			throw new AuthException(AuthErrorCode.NICKNAME_ALREADY_IN_USE);
+		}
+
+		validateSignUpSource(influencerSignup);
+
+		validateSNSAtLeastOneSelected(influencerSignup);
+
+		String encodedPassword = passwordEncoder.encode(influencerSignup.getPw());
+
+		// TODO : 프로필 S3 저장 후 url 가져오기
+
+		User user = User.of(
+				influencerSignup.getEmail(),
+				influencerSignup.getEmail(),
+				encodedPassword,
+				"ROLE_INFLUENCER",
+				influencerSignup.getName(),
+				influencerSignup.getPhone(),
+				"image example",
+				1,
+				null,
+				influencerSignup.getSignupSource().getInfo(),
+				Integer.valueOf(influencerSignup.getPostalCode()),
+				influencerSignup.getAddress(),
+				influencerSignup.getAddressDetail(),
+				0,
+				0,
+				0,
+				0,
+				1
+		);
+		User savedUser = userRepository.save(user);
+
+		Influencer influencer = Influencer.of(
+				savedUser,
+				influencerSignup.getNickname(),
+				influencerSignup.getGender(),
+				LocalDate.parse(influencerSignup.getBirthdate(), DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+				influencerSignup.getBlog(),
+				influencerSignup.getInstagram(),
+				influencerSignup.getYoutube(),
+				influencerSignup.getTiktok(),
+				influencerSignup.getOther()
+		);
+		Influencer savedInfluencer = influencerRepository.save(influencer);
+
+		return UserInfluencerDto.from(savedInfluencer);
+	}
+
+	private static void validateSNSAtLeastOneSelected(InfluencerSignup influencerSignup) {
+		if (influencerSignup.getBlog() == null && influencerSignup.getInstagram() == null && influencerSignup.getYoutube() == null && influencerSignup.getTiktok() == null && influencerSignup.getOther() == null) {
+			throw new ValidException(ValidErrorCode.SNS_DOES_NOT_SELECTED);
+		}
+	}
+
+	private static void validateSignUpSource(InfluencerSignup influencerSignup) {
+		if (!influencerSignup.getSignupSource().equals(SignUpSourceType.PORTAL_SEARCH) && !influencerSignup.getSignupSource().equals(SignUpSourceType.SNS) && !influencerSignup.getSignupSource().equals(SignUpSourceType.INTRODUCTION_TO_ACQUAINTANCES) && !influencerSignup.getSignupSource().equals(SignUpSourceType.ETC)) {
+			throw new ValidException(ValidErrorCode.SIGN_UP_SOURCE_NOT_VALID);
+		}
+	}
 
 }
