90393b059932340c0a046a3eed64a0760cff5650
Reddyong <d0209hong@naver.com>
2024-09-03 17:29:59 +0900

    refactor : 인플루언서 회원가입을 위한 request dto 수정.

--- src/main/java/com/example/finalproject/domain/user/dto/request/InfluencerSignup.java
+++ src/main/java/com/example/finalproject/domain/user/dto/request/InfluencerSignup.java
@@ -1,12 +1,21 @@
 package com.example.finalproject.domain.user.dto.request;
 
+import com.example.finalproject.domain.user.entity.enums.SignUpSourceType;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
+
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(value = PropertyNamingStrategies.LowerCamelCaseStrategy.class)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class InfluencerSignup {
 	private String email;
@@ -14,7 +23,8 @@ public class InfluencerSignup {
 	private String name;
 	private String phone;
 	private String nickname;
-	private String signupSource;
+//	private String signupSource;
+	private SignUpSourceType signupSource;
 
 	private String blog;
 	private String instagram;
@@ -26,7 +36,7 @@ public class InfluencerSignup {
 	private String address;
 	private String addressDetail;
 
-	private MultipartFile profile;
+//	private MultipartFile profile;
 	private String birthdate;
 	private Integer gender;
 
