ed539e3ab5d347332c72213e9e68a057b061cadd
Reddyong <d0209hong@naver.com>
2024-09-03 21:23:48 +0900

    refactor : 사업주 회원가입 기능을 위해 request dto 수정.

--- src/main/java/com/example/finalproject/domain/user/dto/request/BusinessesSignup.java
+++ src/main/java/com/example/finalproject/domain/user/dto/request/BusinessesSignup.java
@@ -1,12 +1,19 @@
 package com.example.finalproject.domain.user.dto.request;
 
+import com.example.finalproject.domain.user.entity.enums.SignUpSourceType;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.web.multipart.MultipartFile;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(value = PropertyNamingStrategies.LowerCamelCaseStrategy.class)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class BusinessesSignup {
 	private String email;
@@ -14,9 +21,10 @@ public class BusinessesSignup {
 	private String name;
 	private String phone;
 	private String company;
-	private String signupSource;
+//	private String signupSource;
+	private SignUpSourceType signupSource;
 	private String postalCode;
 	private String address;
 	private String addressDetail;
-	private MultipartFile profile;
+//	private MultipartFile profile;
 }
