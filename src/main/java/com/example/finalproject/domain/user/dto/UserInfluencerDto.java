6052840024b2d8e322caf9e374f1a9bb127d18ee
Reddyong <d0209hong@naver.com>
2024-09-03 17:32:41 +0900

    refactor : 회원가입 시 인플루언서 dto 내에서 필요없는 속성 주석 처리 및 타입 변경.

--- src/main/java/com/example/finalproject/domain/user/dto/UserInfluencerDto.java
+++ src/main/java/com/example/finalproject/domain/user/dto/UserInfluencerDto.java
@@ -1,19 +1,19 @@
 package com.example.finalproject.domain.user.dto;
 
+import com.example.finalproject.domain.user.entity.Influencer;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import io.swagger.models.auth.In;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonNaming(value = PropertyNamingStrategies.LowerCamelCaseStrategy.class)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserInfluencerDto {
 
@@ -23,27 +23,46 @@ public class UserInfluencerDto {
 
     private String nickname;
 
-    private Date birthdate;
+    private LocalDate birthdate;
 
-    private String Gender;
+    private Integer gender;
 
     private String blogLink;
-    private String blogRank;
-    private Integer blogVisitors;
+//    private String blogRank;
+//    private Integer blogVisitors;
 
     private String instagramLink;
-    private String instagramRank;
-    private Integer instagramFollower;
+//    private String instagramRank;
+//    private Integer instagramFollower;
 
     private String youtubeLink;
-    private String youtubeRank;
-    private Integer youtubeSubscriber;
+//    private String youtubeRank;
+//    private Integer youtubeSubscriber;
 
     private String tiktokLink;
-    private String tiktokRank;
-    private Integer tiktokFollower;
+//    private String tiktokRank;
+//    private Integer tiktokFollower;
 
     private String otherLink;
-    private String otherRank;
+//    private String otherRank;
+
+    public static UserInfluencerDto of(Integer seq, UserDto userDto, String nickname, LocalDate birthdate, Integer gender, String blogLink, String instagramLink, String youtubeLink, String tiktokLink, String otherLink) {
+        return new UserInfluencerDto(seq, userDto, nickname, birthdate, gender, blogLink, instagramLink, youtubeLink, tiktokLink, otherLink);
+    }
+
+    public static UserInfluencerDto from(Influencer influencer) {
+        return UserInfluencerDto.of(
+                influencer.getSeq(),
+                UserDto.from(influencer.getUser()),
+                influencer.getNickname(),
+                influencer.getBirthdate(),
+                influencer.getGender(),
+                influencer.getBlogLink(),
+                influencer.getInstagramLink(),
+                influencer.getYoutubeLink(),
+                influencer.getTiktokLink(),
+                influencer.getOtherLink()
+        );
+    }
 
 }
