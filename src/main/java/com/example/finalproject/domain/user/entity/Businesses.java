17e09fc80b21fcb30ec2c7b28e4eebeb65743f5e
Reddyong <d0209hong@naver.com>
2024-09-03 21:23:14 +0900

    refactor : Businesses 엔티티 생성, 수정, 삭제 일시 속성 추가 및 soft delete 기능 추가.

--- src/main/java/com/example/finalproject/domain/user/entity/Businesses.java
+++ src/main/java/com/example/finalproject/domain/user/entity/Businesses.java
@@ -5,13 +5,19 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@SQLDelete(sql = "UPDATE user_influencer SET deleted_at = NOW() WHERE seq=?")
+@Where(clause = "deleted_at is NULL")
 @Table(name = "user_enterpriser")
 @Builder
 @Entity
@@ -21,7 +27,7 @@ public class Businesses {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer seq;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_seq")
     private User user;
 
@@ -36,4 +42,34 @@ public class Businesses {
 
     @Column(name = "attached_file", length = 50)
     private String attachedFile;
+
+    @Column(name = "registered_at")
+    private Timestamp registeredAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+
+    public Businesses(User user, String company, String businessNumber, String representative) {
+        this.user = user;
+        this.company = company;
+        this.businessNumber = businessNumber;
+        this.representative = representative;
+    }
+
+    public static Businesses of(User user, String company, String businessNumber, String representative) {
+        return new Businesses(user, company, businessNumber, representative);
+    }
+
+    @PrePersist
+    void registeredAt() {
+        this.registeredAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
 }
