0ed42312532cc9f5829acb8dd2672d315f2a0d89
Reddyong <d0209hong@naver.com>
2024-09-06 17:06:32 +0900

    refactor : 배포환경에 맞게 수정.

--- src/main/java/com/example/finalproject/global/config/S3Config.java
+++ src/main/java/com/example/finalproject/global/config/S3Config.java
@@ -13,22 +13,22 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
+//    @Value("${cloud.aws.credentials.access-key}")
+//    private String accessKey;
+//
+//    @Value("${cloud.aws.credentials.secret-key}")
+//    private String secretKey;
 
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Profile("local")
+//    @Profile("local")
     @Bean
     public AmazonS3 amazonS3() {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+//        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
         return AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+//                .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(region)
                 .build();
     }
