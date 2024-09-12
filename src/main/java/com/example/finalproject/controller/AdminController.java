840076ddb531f70611e4e28ec81c20cf9d12703d
Reddyong <d0209hong@naver.com>
2024-09-12 17:04:52 +0900

    feat : 체험단 리스트 기능 컨트롤러 코드 기본 틀 작성.
    * 관리자 메인페이지에서 필요한 기능.

---
+++ src/main/java/com/example/finalproject/controller/AdminController.java
@@ -0,0 +1,43 @@
+package com.example.finalproject.controller;
+
+import com.example.finalproject.domain.campaign.dto.CampaignDto;
+import com.example.finalproject.domain.campaign.dto.response.CampaignListResponse;
+import com.example.finalproject.domain.campaign.entity.enums.FirstCampaignSearchType;
+import com.example.finalproject.domain.campaign.entity.enums.SecondCampaignSearchType;
+import com.example.finalproject.domain.campaign.service.CampaignService;
+import com.example.finalproject.global.util.ResponseApi;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "관리자")
+@RequestMapping("/adm")
+@RequiredArgsConstructor
+@RestController
+public class AdminController {
+
+    private final CampaignService campaignService;
+
+    @ApiOperation(value = "체험 리스트", tags = "관리자 - 체험단")
+    @GetMapping("/campaign")
+    public ResponseApi<Page<CampaignListResponse>> findCampaignList(
+            @RequestParam(required = false) FirstCampaignSearchType searchType1,
+            @RequestParam(required = false) SecondCampaignSearchType searchType2,
+            @RequestParam(required = false) String searchWord,
+            @PageableDefault(sort = "registeredAt", direction = Sort.Direction.DESC) Pageable pageable
+    ){
+        Page<CampaignDto> campaignPage = campaignService.findCampaignPage(searchType1, searchType2, searchWord, pageable);
+
+
+        return ResponseApi.success(HttpStatus.OK, null);
+    }
+}
