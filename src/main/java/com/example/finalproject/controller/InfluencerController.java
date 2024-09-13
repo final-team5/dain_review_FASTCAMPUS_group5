ee97155057f964c0d04a408322b3027d6c63b564
Reddyong <d0209hong@naver.com>
2024-09-13 18:29:51 +0900

    refactor : 배포 swagger 상에 검색 필터 조건 안보임 -> @ApiParam을 통해 설명 추가

--- src/main/java/com/example/finalproject/controller/InfluencerController.java
+++ src/main/java/com/example/finalproject/controller/InfluencerController.java
@@ -75,7 +75,7 @@ public class InfluencerController {
     @GetMapping(path = "/communities")
     public ResponseApi<Page<PostListResponse>> findListInfCommunityPost(
             @RequestParam(required = false) SearchType searchType,
-            @RequestParam(required = false) PostType influencerSearchPostType,
+            @ApiParam(value = "QUESTION, KNOW_HOW, ACCOMPANY, ETC 중 선택", allowableValues = "QUESTION, KNOW_HOW, ACCOMPANY, ETC") @RequestParam(required = false) PostType influencerSearchPostType,
             @RequestParam(required = false) String searchWord,
             @PageableDefault(sort = "registeredAt", direction = Sort.Direction.DESC) Pageable pageable,
             @ApiIgnore @AuthenticationPrincipal UserDetails userDetails
