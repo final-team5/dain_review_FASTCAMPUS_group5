0fa897d32c27222c7e54395dd52e265c12d79bcf
Reddyong <d0209hong@naver.com>
2024-09-14 13:33:01 +0900

    feat : 관리자 메인페이지 리스트 조회기능 컨트롤러 코드 구현

--- src/main/java/com/example/finalproject/controller/AdminController.java
+++ src/main/java/com/example/finalproject/controller/AdminController.java
@@ -8,6 +8,7 @@ import com.example.finalproject.domain.campaign.service.CampaignService;
 import com.example.finalproject.global.util.ResponseApi;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -30,14 +31,14 @@ public class AdminController {
     @ApiOperation(value = "체험 리스트", tags = "관리자 - 체험단")
     @GetMapping("/campaign")
     public ResponseApi<Page<CampaignListResponse>> findCampaignList(
-            @RequestParam(required = false) FirstCampaignSearchType searchType1,
-            @RequestParam(required = false) SecondCampaignSearchType searchType2,
+            @ApiParam(value = "ALL, CAMPAIGN_TITLE, ID, COMPANY_NAME, PHONE_NUMBER 중 선택", allowableValues = "ALL, CAMPAIGN_TITLE, ID, COMPANY_NAME, PHONE_NUMBER") @RequestParam(required = false) FirstCampaignSearchType searchType1,
+            @ApiParam(value = "READY, APPROVE, REFUSAL, CANCEL 중 선택", allowableValues = "READY, APPROVE, REFUSAL, CANCEL") @RequestParam(required = false) SecondCampaignSearchType searchType2,
             @RequestParam(required = false) String searchWord,
             @PageableDefault(sort = "registeredAt", direction = Sort.Direction.DESC) Pageable pageable
     ){
         Page<CampaignDto> campaignPage = campaignService.findCampaignPage(searchType1, searchType2, searchWord, pageable);
+        Page<CampaignListResponse> campaignListResponses = campaignPage.map(CampaignListResponse::from);
 
-
-        return ResponseApi.success(HttpStatus.OK, null);
+        return ResponseApi.success(HttpStatus.OK, campaignListResponses);
     }
 }
