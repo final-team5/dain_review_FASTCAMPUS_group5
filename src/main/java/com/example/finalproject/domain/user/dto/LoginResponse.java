8564869a6e46f94d276c52199bf8167fb1c6ef85
Reddyong <d0209hong@naver.com>
2024-09-19 16:30:13 +0900

    refactor : 로그인 시 회원 프로필 사진 및 알림 갯수 정보도 포함시키기 위해 response 수정.

--- src/main/java/com/example/finalproject/domain/user/dto/LoginResponse.java
+++ src/main/java/com/example/finalproject/domain/user/dto/LoginResponse.java
@@ -1,17 +1,21 @@
 package com.example.finalproject.domain.user.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
 
-@Getter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder
 public class LoginResponse {
 	private String token;
 	private String name;
 	private String expireDate;
+	private String profileUrl;
+	private Integer alarmCounts;
 	private String message;
 }
