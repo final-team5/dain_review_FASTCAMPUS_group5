5f95aa4b8f256c43507a95769841408208ff6439
mango606 <yuj08184@kangwon.ac.kr>
2024-09-20 20:45:20 +0900

    refactor: 체험단 신청 및 취소 Request 값 수정

--- src/main/java/com/example/finalproject/domain/campaign/dto/request/CampaignApplicantsRequest.java
+++ src/main/java/com/example/finalproject/domain/campaign/dto/request/CampaignApplicantsRequest.java
@@ -1,5 +1,6 @@
 package com.example.finalproject.domain.campaign.dto.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CampaignApplicantsRequest {
+
+    @ApiModelProperty(value = "사용자 번호", example = "6", required = true)
     private Integer userSeq;
+
+    @ApiModelProperty(value = "체험단 번호", example = "8", required = true)
     private Integer campaignSeq;
+
+    @ApiModelProperty(value = "신청 메시지", example = "체험단 신청 메시지입니다.", required = true)
     private String message;
 }
--- src/main/java/com/example/finalproject/domain/campaign/dto/request/CancelCampaignRequest.java
+++ src/main/java/com/example/finalproject/domain/campaign/dto/request/CancelCampaignRequest.java
@@ -1,5 +1,6 @@
 package com.example.finalproject.domain.campaign.dto.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CancelCampaignRequest {
+
+    @ApiModelProperty(value = "사용자 번호", example = "6", required = true)
     private Integer userSeq;
+
+    @ApiModelProperty(value = "체험단 번호", example = "8", required = true)
     private Integer campaignSeq;
+
+    @ApiModelProperty(value = "취소 사유", example = "개인적인 사유로 인해 신청을 취소합니다.", required = true)
     private String reason;
 }
